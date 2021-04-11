239708f1a2f3c3d8b8a2dcd54f1da9bb69fcfc2d
vuthanh199826 <vuthanh199825@gmail.com>
2021-04-12 00:24:27 +0700

    TH vonglap for

---
+++ js/TH_su_dung_vong_lap_for.js
@@ -0,0 +1,14 @@
+// let text = "";
+// let i;
+//
+// for (i = 0; i < 5; i++) {
+//     text += "The number is " + i + "<br>";
+// };
+// document.getElementById("demo").innerHTML = text;
+let n = +prompt("nhập n vào đây");
+let i;
+let sum = 0;
+for(i=0; i<n ; i++){
+    sum = sum + i ;
+};
+alert(sum);
